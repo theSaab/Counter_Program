56898c0c1168deaa3dc011ccf406af06fb11b41f
Saba Fathi <saba.fathi@emailgmail.info>
2020-05-29 16:12:15 -0400

    annoying

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,34 @@
 
-
 import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 0
     };
 
-    styles = {
-        fontSize: 10,
-        fontWeight: '10px',
-    };
+    handle_increment = product => {
+        console.log(product);
+        this.setState({ count: this.state.count + 1 })
+    }
 
     render() {
         return (
             <div>
-                <span style={{fontSize: 30} } className='badge badge-primary m-2'>{this.formatCount()}</span>
-                <button className='btn btn-secondary btn-sm'>Increment</button>
+                <span className={this.get_badge_classes()}>{this.formatCount()}</span>
+                <button
+                    onClick={() => this.handle_increment()}
+                    className='btn btn-secondary btn-'>Increment
+                </button>
             </div>
-        )
+        );
     }
+
+    get_badge_classes() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
     formatCount() {
         const { count } = this.state;
         return count === 0 ? 'Zero' : count;
